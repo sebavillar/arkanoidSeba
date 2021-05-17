09eb7ff5e10ff3ac7d813d17e35647e36a681344
Juan Sebastian Villar <sebavillar@gmail.com>
2021-05-16 22:01:40 -0300

    Creamos ladrillos

---
+++ Engine/Brick.cpp
@@ -0,0 +1,15 @@
+#include "Brick.h"
+
+Brick::Brick(const RectF& rect_in, Color color_in)
+	:
+	rect(rect_in),
+	color(color_in)
+
+{
+}
+
+void Brick::Draw(Graphics& gfx) const
+{
+	gfx.DrawRect(rect, color);
+
+}
