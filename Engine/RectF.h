3af5947c196c6f9a65000221df03c9006eb0ed4c
Juan Sebastian Villar <sebavillar@gmail.com>
2021-05-18 22:33:15 -0300

    Cambio RectF

--- Engine/RectF.h
+++ Engine/RectF.h
@@ -8,7 +8,9 @@ public:
 	RectF(float left_in, float right_in, float top_in, float bottom_in);
 	RectF(const Vec2& topLeft, const Vec2& bottomRight);
 	RectF(const Vec2& topLeft, float width, float height);
+	
 	bool IsOverlappingWith(const RectF& other) const;
+	static RectF FromCenter(const Vec2& center, float halfWidth, float halfHeight);
 
 public:
 	float left;
